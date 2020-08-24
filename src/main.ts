3038f4b9a580b3f0d46b4b1c26d39d7e7803ada7
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-08-24 19:58:05 +0000

    Listen to port from environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT);
 
   // const app2 = await NestFactory.create(AppModule);
   // await app2.listen(4000);
