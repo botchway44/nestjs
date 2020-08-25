6215f0f8d30a75372b5dd28865b46d361bfbe982
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-08-25 05:46:28 +0000

    added docs to resolvers

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,23 +1,35 @@
 import { Column, PrimaryGeneratedColumn, Entity } from 'typeorm';
 import { TaskStatus } from '../model/taskstatus';
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
 
 @Entity()
 @ObjectType()
 export class Task {
   @PrimaryGeneratedColumn()
-  @Field((type) => Int)
+  @Field((type) => ID, {
+    name: 'id',
+    description: 'ID of a specific task ',
+  })
   id: number | undefined;
 
   @Column()
-  @Field((type) => String)
+  @Field((type) => String, {
+    name: 'title',
+    description: 'title of a task ',
+  })
   title: string | undefined;
 
   @Column()
-  @Field((type) => String)
+  @Field((type) => String, {
+    name: 'description',
+    description: 'description of a specific task ',
+  })
   description: string | undefined;
 
   @Column()
-  @Field((type) => TaskStatus)
+  @Field((type) => TaskStatus, {
+    name: 'status',
+    description: 'status of a specific task in any of the 3 states',
+  })
   status: TaskStatus | undefined;
 }
--- src/task/resolvers/task.resolver.ts
+++ src/task/resolvers/task.resolver.ts
@@ -8,25 +8,37 @@ import { TaskStatus } from '../model/taskstatus';
 export class TaskResolver {
   constructor(private taskService: PostgresTaskService) {}
 
-  @Query(() => [Task])
+  @Query(() => [Task], {
+    name: 'getAllTask',
+    description: 'Returns all the task for a particular User',
+  })
   async allTask(): Promise<Task[]> {
     return this.taskService.getAllTask();
   }
 
-  @Query(() => Task)
+  @Query(() => Task, {
+    name: 'getTaskById',
+    description: 'Returns all the task for a particular User by a given ID',
+  })
   async getTaskById(
     @Args('id', { type: () => Int }) id: number,
   ): Promise<Task> {
     return this.taskService.getTaskByID(id);
   }
 
-  @Mutation(() => Task)
+  @Mutation(() => Task, {
+    name: 'deleteTaskById',
+    description: 'Deletes a specific task for a particular User by a given ID',
+  })
   deleteTaskById(@Args('id', { type: () => Int }) id: number): Promise<Task> {
     console.log(`Remove Task ${id} `);
     return this.taskService.deleteTaskByID(id);
   }
 
-  @Mutation(() => Task)
+  @Mutation(() => Task, {
+    name: 'addTask',
+    description: "Adds task to a Specific User's List of Tasks",
+  })
   addTask(
     @Args('createTaskDTO', { type: () => CreateTaskDTO })
     createTaskDTO: CreateTaskDTO,
@@ -34,11 +46,13 @@ export class TaskResolver {
     return this.taskService.addTask(createTaskDTO);
   }
 
-  @Mutation(() => Task)
+  @Mutation(() => Task, {
+    name: 'updateTask',
+    description: "Updates a  task for a Specific User's List of Tasks",
+  })
   updateTask(
     @Args('status', { type: () => TaskStatus })
     status: TaskStatus,
-
     @Args('id', { type: () => Int }) id: number,
   ): Promise<Task> {
     return this.taskService.updateTaskStatus(id, status);
